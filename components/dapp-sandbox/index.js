1fe3b56e1760952af8574eb4cbf9fdaa0cc3aaea
kumavis <aaron@kumavis.me>
2015-07-01 01:11:08 -0700

    wallet - auto tx signing

--- components/dapp-sandbox/index.js
+++ components/dapp-sandbox/index.js
@@ -1,5 +1,6 @@
 const IframeSandbox = require('iframe-sandbox')
 const XhrStream = require('xhr-stream')
+const request = require('request')
 const anchor = require('../router/index.js').anchor
 const hg = require('../../mercury.js')
 const h = require('../../mercury.js').h
@@ -8,7 +9,11 @@ const mustOverride = require('../../util/mustOverride.js')
 const stateExtend = require('../../util/stateExtend.js')
 const LifecycleHook = require('../../util/lifecycleHook.js')
 // TODO: should be an env var
-const dappTransformUrl = '//transform.'+location.host+'/'
+// const dappTransformUrl = '//transform.'+location.host+'/'
+const dappTransformUrl = 'https://transform.vapor.to/'
+// const rpcUrl = '//rpc.'+location.host+'/'
+const rpcUrl = 'https://rpc.vapor.to/'
+// const rpcUrl = 'http://localhost:8000/'
 
 module.exports = Component
 
@@ -91,13 +96,17 @@ function requestDappByUrl(url) {
   return new XhrStream( xhr )
 }
 
-function handleRpc(message) {
-  console.log('vapor heard rpc:', message.method, message.params)
-  // web3.eth.getTransactionCount(walletAddress, _handleRpc.bind(null, message))
-}
+// rpc stuff
+
+// var Transaction = require('ethereumjs-tx')
+var Transaction = require('ethereumjs-lib').Transaction
+
+// function handleRpc(message) {
+//   console.log('vapor heard rpc:', message.method, message.params)
+//   // web3.eth.getTransactionCount(walletAddress, _handleRpc.bind(null, message))
+// }
 
 // import ethereumjslib from 'npm:ethereumjs-lib'
-// var Transaction = ethereumjslib.Transaction
 // import web3 from 'npm:web3'
 // import BufferModule from 'npm:buffer'
 // var Buffer = BufferModule.Buffer
@@ -105,74 +114,100 @@ function handleRpc(message) {
 
 // web3.setProvider(new web3.providers.HttpProvider(vaporRpcUrl))
 
-// var walletAddress = 'a06ef3ed1ce41ade87f764de6ce8095c569d6d57'
-// var walletKey = Buffer('8234b7fb702abf568633b91b22c03bf9344a6b5371651623d6c412c5f8d9ba73', 'hex')
+var walletAddress = Buffer('a06ef3ed1ce41ade87f764de6ce8095c569d6d57')
+var walletKey = Buffer('8234b7fb702abf568633b91b22c03bf9344a6b5371651623d6c412c5f8d9ba73', 'hex')
+
+function handleRpc(message){
+  console.log('vapor heard rpc:', message.method, message.params)
+
+  getNonce(walletAddress, function(err, txCount){
+
+    switch(message.method) {
+      
+      case 'eth_sendTransaction':
+        var txParams = message.params[0]
+        txParams.to = txParams.to ? normalizeHexString(txParams.to) : null
+        txParams.value = txParams.value ? normalizeHexString(txParams.value) : null
+        txParams.data = txParams.data ? normalizeHexString(txParams.data) : null
+        txParams.nonce = txCount
+        txParams.gasLimit = txParams.gas ? normalizeHexString(txParams.gas) : '2fefd8'
+        txParams.gasPrice = 1
+
+        console.log('sending signed tx:', txParams)
+        var tx = new Transaction(txParams)
+        tx.sign(walletKey)
+        sendSignedTransaction(tx)
+        return
+
+      default:
+        return
+
+      }
+
+    })
+
+}
 
 
-// function _handleRpc(message, err, txCount){
-  
-//   switch(message.method) {
-    
-//     case 'eth_sendTransaction':
-//       var txParams = message.params[0]
-//       txParams.to = txParams.to ? normalizeHexString(txParams.to) : null
-//       txParams.value = txParams.value ? normalizeHexString(txParams.value) : null
-//       txParams.data = txParams.data ? normalizeHexString(txParams.data) : null
-//       txParams.nonce = txCount
-//       txParams.gasLimit = txParams.gas ? normalizeHexString(txParams.gas) : '2fefd8'
-//       txParams.gasPrice = 1
+// ==========================
 
-//       console.log('sending signed tx:', txParams)
-//       var tx = new Transaction(txParams)
-//       signTx(tx, walletKey)
-//       sendSignedTransaction(tx.serialize().toString('hex'))
-//       return
+function sendSignedTransaction(signedTx) {
+  var data = signedTx.serialize().toString('hex')
+  var includeTrace = window.DEBUG_RPC || false
+  sendRpc('eth_signedTransact', [data, includeTrace])
+}
 
-//     default:
-//       return
+function getNonce(address, cb) {
 
-//   }
+  var data = address.toString('hex')
+  sendRpc('eth_getTransactionCount', data, function(err, res, body){
+    if (err) throw err
+    if (body.error) throw body.error
+    nonce = body.result
+    if (nonce === '0x') {
+      nonce = '0x00'
+    }
+    console.log('nonce:', nonce)
+    cb(null, nonce)
+  })
 
-// }
+}
 
+function sendRpc(method, data, cb) {
+  cb = cb || noop
 
-// // ==========================
+  var rpcPayload = {
+    id: getRandomId(),
+    jsonrpc: '2.0',
+    method: method,
+    params: [data],
+  }
 
-// var MAX_SAFE_INTEGER = Math.pow(2, 53) - 1
+  request.post(rpcUrl, { withCredentials: false, json: rpcPayload }, cb)
+}
 
-// function signTx(tx, walletKey) {
-//   tx.sign(walletKey)
-// }
+// ==========================
 
-// function sendSignedTransaction(signedTx) {
-//   var stackTrace = window.DEBUG_RPC || false
-//   var msgId = Math.floor(Math.random() * MAX_SAFE_INTEGER)
-//   var payload = {
-//     'method': 'eth_signedTransact',
-//     'params': [signedTx, stackTrace],
-//     'id': msgId,
-//     'jsonrpc': '2.0',
-//   }
+var MAX_SAFE_INTEGER = Math.pow(2, 53) - 1
 
-//   var request = new XMLHttpRequest();
-//   request.open('POST', vaporRpcUrl, true);
-//   request.send(JSON.stringify(payload));
-// }
+function normalizeHexString(hex) {
+  if (!hex) {
+    return hex
+  }
 
-// // ==========================
+  if (hex.slice(0, 2) === '0x') {
+    hex = hex.slice(2);
+  }
 
-// function normalizeHexString(hex) {
-//   if (!hex) {
-//     return hex
-//   }
+  if (hex.length%2 !== 0) {
+    hex = '0'+hex
+  }
 
-//   if (hex.slice(0, 2) === '0x') {
-//     hex = hex.slice(2);
-//   }
+  return hex
+}
 
-//   if (hex.length%2 !== 0) {
-//     hex = '0'+hex
-//   }
+function getRandomId(){
+  return Math.floor(Math.random() * MAX_SAFE_INTEGER)
+}
 
-//   return hex
-// }
+function noop(){}
