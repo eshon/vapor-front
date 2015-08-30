07ca7beb6a4ef44c03265e1332d38458eca11245
kumavis <aaron@kumavis.me>
2015-08-30 14:48:55 -0700

    beta - point iframe directly at transform

--- components/dapp-sandbox/index.js
+++ components/dapp-sandbox/index.js
@@ -1,3 +1,4 @@
+const iframe = require('iframe')
 const IframeSandbox = require('iframe-sandbox')
 const XhrStream = require('xhr-stream')
 const anchor = require('../router/index.js').anchor
@@ -60,121 +61,129 @@ function didInsertElement(state, container) {
     currentSandboxIframeUrl = null
   }
 
-  var frameConfig = {
-    container: container,
-    origin: target,
-  }
+  // var frameConfig = {
+  //   container: container,
+  //   origin: target,
+  // }
 
   // bug - doesnt work without this 
   process.nextTick(function(){
 
     // start sandbox
     console.log('starting sandbox for "'+target+'"...')
-    IframeSandbox(frameConfig, function(err, sandbox) {
-      if (err) return console.error(err)
-
-      // load dapp into sandbox DOM
-      console.log('loading "'+target+'"...')
-      requestDappByUrl( target )
-        .pipe( sandbox.createWriteStream() )
-
-      // handle messages
-      sandbox.on('message', handleSandboxMessage)
-
+    iframe({
+      container: container,
+      src: urlForHtmlTransform( target ),
     })
+    // IframeSandbox(frameConfig, function(err, sandbox) {
+    //   if (err) return console.error(err)
+
+    //   // load dapp into sandbox DOM
+    //   console.log('loading "'+target+'"...')
+    //   requestDappByUrl( target )
+    //     .pipe( sandbox.createWriteStream() )
+
+    //   // handle messages
+    //   sandbox.on('message', handleSandboxMessage)
+
+    // })
 
   })
+}
+
+function urlForHtmlTransform(url) {
+  return dappTransformUrl + '/html/' + encodeURIComponent(url)
 }
 
 function willRemoveElement(state, container) {
-  var target = state.dappUrl
-  var iframe = container.childNodes[0]
+//   var target = state.dappUrl
+//   var iframe = container.childNodes[0]
 
-  // iframe may be recreated immediately
-  // due to an aggressive virtual-dom
-  // so we keep these references around
-  currentSandboxIframe = iframe
-  currentSandboxIframeInTransit = true
+//   // iframe may be recreated immediately
+//   // due to an aggressive virtual-dom
+//   // so we keep these references around
+//   currentSandboxIframe = iframe
+//   currentSandboxIframeInTransit = true
 
-  // hide iframe so it doesnt cause a rendering blip
-  // in the case where we dont reuse it 
-  iframe.style.display = 'none'
+//   // hide iframe so it doesnt cause a rendering blip
+//   // in the case where we dont reuse it 
+//   iframe.style.display = 'none'
 
-  // check if we did not reuse the iframe
-  process.nextTick(function(){
-    if (!currentSandboxIframeInTransit) return
-    // iframe is still in transit, was not reinserted, need to cleanup
-    currentSandboxIframeInTransit = false
-    container.removeChild(iframe)
-  })
+//   // check if we did not reuse the iframe
+//   process.nextTick(function(){
+//     if (!currentSandboxIframeInTransit) return
+//     // iframe is still in transit, was not reinserted, need to cleanup
+//     currentSandboxIframeInTransit = false
+//     container.removeChild(iframe)
+//   })
 }
 
-//
-// Sandbox methods
-//
-function handleSandboxMessage(message) {
-  // handle rpc call
-  if (message.jsonrpc || (message[0] && message[0].jsonrpc)) {
-    // batched call
-    if (Array.isArray(message)) {
-      message.forEach(handleRpc)
-    // single call
-    } else {
-      handleRpc(message)            
-    }
-  // unknown message
-  } else {
-    console.warn('Vapor - Unknown message from sandbox:', message)
-  }
-}
+// //
+// // Sandbox methods
+// //
+// function handleSandboxMessage(message) {
+//   // handle rpc call
+//   if (message.jsonrpc || (message[0] && message[0].jsonrpc)) {
+//     // batched call
+//     if (Array.isArray(message)) {
+//       message.forEach(handleRpc)
+//     // single call
+//     } else {
+//       handleRpc(message)            
+//     }
+//   // unknown message
+//   } else {
+//     console.warn('Vapor - Unknown message from sandbox:', message)
+//   }
+// }
 
-function requestDappByUrl(url) {
-  var xhr = new XMLHttpRequest()
-  url = dappTransformUrl + '/' + encodeURIComponent(url)
-  xhr.open('GET', url, true)
-  return new XhrStream( xhr )
-}
+// function requestDappByUrl(url) {
+//   var xhr = new XMLHttpRequest()
+//   url = dappTransformUrl + '/' + encodeURIComponent(url)
+//   xhr.open('GET', url, true)
+//   return new XhrStream( xhr )
+// }
 
-// rpc stuff
+// // rpc stuff
 
-function handleRpc(message){
-  console.log('vapor heard rpc:', message.method, message.params)
+// function handleRpc(message){
+//   console.log('vapor heard rpc:', message.method, message.params)
 
-    switch(message.method) {
+//     switch(message.method) {
       
-      case 'eth_sendTransaction':
-        var txParams = message.params[0]
-        txParams.to = txParams.to ? normalizeHexString(txParams.to) : null
-        txParams.value = txParams.value ? normalizeHexString(txParams.value) : null
-        txParams.data = txParams.data ? normalizeHexString(txParams.data) : null
-        txParams.gasLimit = txParams.gas ? normalizeHexString(txParams.gas) : '2fefd8'
-        txParams.gasPrice = 1
+//       case 'eth_sendTransaction':
+//         var txParams = message.params[0]
+//         txParams.to = txParams.to ? normalizeHexString(txParams.to) : null
+//         txParams.value = txParams.value ? normalizeHexString(txParams.value) : null
+//         txParams.data = txParams.data ? normalizeHexString(txParams.data) : null
+//         txParams.gasLimit = txParams.gas ? normalizeHexString(txParams.gas) : '2fefd8'
+//         txParams.gasPrice = 1
 
-        addPendingTx(txParams)
-        return
+//         addPendingTx(txParams)
+//         return
 
-      default:
-        return
+//       default:
+//         return
 
-      }
-}
+//       }
+// }
 
-function addPendingTx(txParams) {
-  currentFlatSandbox.actions.newPendingTx(txParams)
-}
+// function addPendingTx(txParams) {
+//   currentFlatSandbox.actions.newPendingTx(txParams)
+// }
 
-function normalizeHexString(hex) {
-  if (!hex) {
-    return hex
-  }
+// function normalizeHexString(hex) {
+//   if (!hex) {
+//     return hex
+//   }
 
-  if (hex.slice(0, 2) === '0x') {
-    hex = hex.slice(2);
-  }
+//   if (hex.slice(0, 2) === '0x') {
+//     hex = hex.slice(2);
+//   }
 
-  if (hex.length%2 !== 0) {
-    hex = '0'+hex
-  }
+//   if (hex.length%2 !== 0) {
+//     hex = '0'+hex
+//   }
 
-  return hex
-}
+//   return hex
+// }
