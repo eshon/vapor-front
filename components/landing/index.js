a256fb249c3cd238d1dd07b971a7385a6e30c33c
eshon <eva.shon@consensys.net>
2015-11-02 16:35:25 -0500

    quick design skin

--- components/landing/index.js
+++ components/landing/index.js
@@ -15,12 +15,13 @@ Component.render = function render(state) {
 function aboutSection(){
   return h('.app-section-about.flex-row.flex-center', [
     h('.flex-column', [
-      h('h2', 'Your window into the decentralized web'),
+      h('h1.logo'),
+      h('h2', 'Your window into the decentralized web.'),
       h('br'),
-      h('span', 'A new paradigm is upon us.'),
-      h('span', 'The internet is reinventing itself.'),
-      h('span', 'We offer ourselves as humble gatekeepers'),
-      h('span', 'to a new ecosystem of human collaboration.'),
+      h('span.desc', 'A new paradigm is upon us.'),
+      h('span.desc', 'The internet is reinventing itself.'),
+      h('span.desc', 'We offer ourselves as humble gatekeepers'),
+      h('span.desc', 'to a new ecosystem of human collaboration.'),
     ]),
   ])
 }
