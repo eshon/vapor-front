4ab34a10e3a4d1a6f6d85f157025580fd697af7a
kumavis <aaron@kumavis.me>
2015-11-06 01:34:07 -0800

    app-bar - notification - small polish

--- components/notification-tx/index.js
+++ components/notification-tx/index.js
@@ -21,21 +21,28 @@ Component.render = function render(state, submitTx, removeTx, setTxFrom) {
   var d = HyperDrive('.notification.flex-row.flex-space-between')
   var fromOptions = keyManager.observ.identities().map(function(id){ return id.label })
   
+  d('span', 'Unconfirmed Tx:')
   if (fromOptions.length) {
     d('from: ')
     d(selectComponent(fromOptions, setTxFrom.bind(null, state)))
   } else {
-    d(anchor({ href: '/identities/' }, 'unlock identities'))
+    d(anchor({ href: '/identities/' }, 'from: unlock identities'))
   }
 
   var isValid = !!fromOptions.length
 
-  d('span', state.to ? 'to: '+state.to.slice(0,8) : 'new contract')
-  d('span', 'ether: '+parseInt(state.value, 16))
+  d('span', 'to: '+(state.to ? state.to.slice(0,8) : 'new contract'))
+  d('span', 'value: '+parseInt(state.value || '0', 16))
+  d('span', state.data ? 'data size: '+dataSize(state.data) : '')
   d('.flex-row-right', [
     h('button', { 'ev-click': hg.sendClick(submitTx, state), disabled: !isValid }, 'confirm'),
     h('button', { 'ev-click': hg.sendClick(removeTx, state) }, 'cancel'),
   ])
 
   return d.render()
+}
+
+function dataSize(data){
+  var size = (data.length-2)/2/1024
+  return size.toString()+' kB'
 }
